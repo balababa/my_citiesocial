c3a03688caf0e366d5d20387ac33ab6ba7818379
cool8676 <cool8676@gmail.com>
2020-03-25 20:10:11 +0800

    Add elements after logging in and flash

--- app/javascript/scripts/shared/application.js
+++ app/javascript/scripts/shared/application.js
@@ -22,5 +22,16 @@ document.addEventListener('turbolinks:load', () => {
       });
     });
   }
-
 });
+
+
+  // close flash
+document.addEventListener("turbolinks:load", () => {  
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    $notification = $delete.parentNode;
+
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+})
