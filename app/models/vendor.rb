e42d3e2af50998e54ef8b8a058cf930f7e9a0c01
cool8676 <cool8676@gmail.com>
2020-03-27 01:32:22 +0800

    add form for Product

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,10 @@
 class Vendor < ApplicationRecord
   acts_as_paranoid
   validates :title, presence: true
+
+  scope :available, -> { where(online: true) }
+  # def self.available
+  #   self.where(online: true)
+  # end
+  
 end
