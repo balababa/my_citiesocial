06f27ef40695b73c6878767270ce76ec54e7193e
cool8676 <cool8676@gmail.com>
2020-03-31 00:55:49 +0800

    Finish draggable category list backend

--- app/javascript/scripts/backend/sortable.js
+++ app/javascript/scripts/backend/sortable.js
@@ -1,4 +1,6 @@
 import Sortable from 'sortablejs';
+import Rails from "@rails/ujs";
+
 
 document.addEventListener('turbolinks:load', (event) => {
   let list = document.querySelector('.list-group');
@@ -7,11 +9,27 @@ document.addEventListener('turbolinks:load', (event) => {
     Sortable.create(list, { 
       animation: 150,
       onEnd: function (evt) {
-
+        if (evt.oldIndex == evt.newIndex) {
+          return
+        }
         let [model, id] = evt.item.dataset.item.split('_');
-        
-        console.log(model,id)
-        console.log(evt.oldIndex, evt.newIndex)
+        let data = new FormData();
+        data.append("id", id);
+        data.append("from", evt.oldIndex);
+        data.append("to", evt.newIndex);
+
+        Rails.ajax({
+          url: "/admin/categories/sort",
+          type: "PUT",
+          dataType: 'json',
+          data,
+          success: response => {
+            console.log(response)
+          },
+          error: err => {
+            console.log(response)
+          }
+        })  
 
       },
     });
